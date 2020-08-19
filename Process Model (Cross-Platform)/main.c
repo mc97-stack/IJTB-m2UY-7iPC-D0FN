171e9c1a0fc03a5310ef628f6cf581939d144eb0
mc97-stack <matthew.cheung97@icloud.com>
2020-08-19 04:58:34 +0100

    Initial Commit

---
+++ Process Model (Cross-Platform)/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Process Model (Cross-Platform)
+//
+//  Created by Matthew Cheung on 19/08/2020.
+//  Copyright © 2020 Matthew Cheung. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
