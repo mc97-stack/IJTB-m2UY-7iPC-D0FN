a7c3643575e0ed4692d55f1089065e76c545404b
mc97-stack <matthew.cheung97@icloud.com>
2020-10-20 19:51:56 +0100

    This commit contains the header file for "OpenFirstLaw.c" to prevent linker errors should the functions in this file be utilised elsewhere in the program.

--- Process Model (Cross-Platform)/Course Material/Year 2/5. B48BC - Process Engineering B (Thermodynamic Processes)/Topic 2/02cOpenFirstLaw.h
+++ Process Model (Cross-Platform)/Course Material/Year 2/5. B48BC - Process Engineering B (Thermodynamic Processes)/Topic 2/02cOpenFirstLaw.h
@@ -12,9 +12,33 @@ typedef struct T2StateEnergy{
     double potenergy;
 } T2StateEnergy;
 
-#ifndef _2cOpenFirstLaw_h
-#define _2cOpenFirstLaw_h
+#ifndef OpenFirstLawVarProc_h
+#define OpenFirstLawVarProc_h
 
-#include <stdio.h>
+/// This subroutine collects values associated with the unit operation under examination.
+/// @param q Specific process heat (kJ/ kmol)
+/// @param w_s Specific shaft work (kJ/ kmol)
+void OpenFirstLawVarProc(double *q, double *w_s);
 
-#endif /* _2cOpenFirstLaw_h */
+#endif /* OpenFirstLawVarProc_h */
+
+#ifndef OpenFirstLawVar_h
+#define OpenFirstLawVar_h
+
+/// This subroutine collects values associated with the fluid streams at a particular time state.
+/// @param ins Timestate indicator
+T2StateEnergy OpenFirstLawVar(int ins);
+
+#endif /* OpenFirstLawVar_h */
+
+#ifndef OpenFirstLawCalc_h
+#define OpenFirstLawCalc_h
+
+/// This subroutine is used to calculate the state of the open system.
+/// @param q Specific process heat
+/// @param w_s Specific shaft work
+/// @param state1 Fluid energy content at state 1
+/// @param state2 Fluid energy content at state 2
+double OpenFirstLawCalc(double q, double w_s, T2StateEnergy state1, T2StateEnergy state2);
+
+#endif /* OpenFirstLawCalc_h */
