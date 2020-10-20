5696bfbb5bb09145761f2f3155157ebda86ee6f0
mc97-stack <matthew.cheung97@icloud.com>
2020-10-20 17:47:18 +0100

    This commit contains an update to "IdealGasLaw.c" to include equations allowing it to use a specific gas constant instead of the universal gas constant. This commit also contains the changes to the appropriate header file as well.

--- Process Model (Cross-Platform)/Course Material/Common subroutines/IdealGasLaw.c
+++ Process Model (Cross-Platform)/Course Material/Common subroutines/IdealGasLaw.c
@@ -14,9 +14,21 @@
 //  Custom header files
 #include "IdealGasLaw.h"
 
-#define R 8.31455
-
 double IdealPressure(double n, double T, double V)
+{
+    double P = 0.0;
+    double R = 0.0;
+    
+    R = 8.31455;
+    
+    P = n*R;
+    P = (P)*T;
+    P = (P)/V;
+    
+    return P;
+}
+
+double SpecPressure(double n, double T, double V, double R)
 {
     double P = 0.0;
     
@@ -28,6 +40,24 @@ double IdealPressure(double n, double T, double V)
 }
 
 double IdealTemperature(double n, double P, double V)
+{
+    double T = 0.0;
+    double R = 0.0;
+    
+    R = 8.31455;
+    
+    double top = 0.0;
+    double bot = 0.0;
+    
+    top = P*V;
+    bot = n*R;
+    
+    T = (top)/(bot);
+    
+    return T;
+}
+
+double SpecTemperature(double n, double P, double V, double R)
 {
     double T = 0.0;
     
@@ -43,6 +73,20 @@ double IdealTemperature(double n, double P, double V)
 }
 
 double IdealVolume(double n, double P, double T)
+{
+    double V = 0.0;
+    double R = 0.0;
+    
+    R = 8.31455;
+    
+    V = n*R;
+    V = (V)*T;
+    V = (V)/P;
+    
+    return V;
+}
+
+double SpecVolume(double n, double P, double T, double R)
 {
     double V = 0.0;
     
--- Process Model (Cross-Platform)/Course Material/Common subroutines/IdealGasLaw.h
+++ Process Model (Cross-Platform)/Course Material/Common subroutines/IdealGasLaw.h
@@ -17,6 +17,18 @@ double IdealPressure(double n, double T, double V);
 
 #endif /* IdealPressure_h */
 
+#ifndef SpecPressure_h
+#define SpecPressure_h
+
+/// Subroutine to calculate the pure component pressure from rearrangement of the ideal gas law applied to a real gas. Function returns the pure component pressure.
+/// @param n Moles of substance
+/// @param T System Temperature
+/// @param V Volume of Component
+/// @param R Specific gas constant
+double SpecPressure(double n, double T, double V, double R);
+
+#endif /* SpecPressure_h */
+
 #ifndef IdealTemperature_h
 #define IdealTemperature_h
 
@@ -26,7 +38,19 @@ double IdealPressure(double n, double T, double V);
 /// @param V Volume of Component
 double IdealTemperature(double n, double P, double V);
 
-#endif /* IdealPressure_h */
+#endif /* IdealTemperature_h */
+
+#ifndef SpecTemperature_h
+#define SpecTemperature_h
+
+/// Subroutine to calculate the system temperature from rearrangement of the ideal gas law. Funtion returns the system temperature
+/// @param n Moles of substance
+/// @param P Component partial pressure
+/// @param V Volume of Component
+/// @param R Specific gas constant
+double SpecTemperature(double n, double P, double V, double R);
+
+#endif /* SpecTemperature_h */
 
 #ifndef IdealVolume_h
 #define IdealVolume_h
@@ -37,4 +61,16 @@ double IdealTemperature(double n, double P, double V);
 /// @param T System Temperature
 double IdealVolume(double n, double P, double T);
 
-#endif /* IdealPressure_h */
+#endif /* IdealVolume_h */
+
+#ifndef SpecVolume_h
+#define SpecVolume_h
+
+/// Subroutine to calculate the system volume from rearrangement of the ideal gas law. Function returns the pure component volume
+/// @param n Moles of substance
+/// @param P Component partial pressure
+/// @param T System Temperature
+/// @param R Specific gas constant
+double SpecVolume(double n, double P, double T, double R);
+
+#endif /* SpecVolume_h */
